8651abb765a351946788c903e85e04785851c547
Alex Suraci <i.am@toogeneric.com>
2011-02-24 15:24:14 -0500

    allow constant namespacing, e.g. Foo::Bar

--- lib/atomo/ast/constant.rb
+++ lib/atomo/ast/constant.rb
@@ -7,22 +7,29 @@ module Atomo
         "constant"
       end
 
-      def initialize(name)
+      def initialize(name, subs = [])
         @name = name
+        @subs = subs
         @line = 1 # TODO
       end
 
       attr_reader :name
 
       def self.grammar(g)
-        g.constant = g.lit(/[A-Z][a-zA-Z0-9_]*/) do |str|
-          Constant.new(str)
+        g.constant = g.seq(
+          g.lit(/[A-Z][a-zA-Z0-9_]*/),
+          g.kleene(g.seq("::", g.t(/[A-Z][a-zA-Z0-9_]*/)))
+        ) do |main, subs|
+          Constant.new(main, Array(subs))
         end
       end
 
       def bytecode(g)
         pos(g)
         g.push_const @name.to_sym
+        @subs.each do |s|
+          g.find_const s.to_sym
+        end
       end
     end
   end
