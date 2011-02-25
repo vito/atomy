53c7b617929dc55cd30ec5ec00006b6648b5a028
Alex Suraci <i.am@toogeneric.com>
2011-02-24 21:09:20 -0500

    updated constant patterns

--- lib/atomo/patterns/constant.rb
+++ lib/atomo/patterns/constant.rb
@@ -1,15 +1,20 @@
 module Atomo::Patterns
   class Constant < Pattern
-    def initialize(name)
-      @name = name
+    attr_reader :chain
+
+    def initialize(chain)
+      @chain = chain
     end
 
     def target(g)
-      g.push_const @name.to_sym
+      g.push_const @chain[0].to_sym
+      @chain.drop(1).each do |n|
+        g.find_const n.to_sym
+      end
     end
 
     def matches?(g)
-      g.push_const @name.to_sym
+      target(g)
       g.swap
       g.kind_of
     end
@@ -18,7 +23,7 @@ module Atomo::Patterns
       g.push_const :Atomo
       g.find_const :Patterns
       g.find_const :Constant
-      g.push_literal @name
+      g.push_literal @chain
       g.send :new, 1
     end
   end
