3ac674acfde0ee1b31c1b6ce9eb0ade152cb9816
Alex Suraci <i.am@toogeneric.com>
2011-02-26 18:46:16 -0500

    class declaration/reopening macros. closes #11.

---
+++ lib/atomo/ast/internal/class.rb
@@ -0,0 +1,32 @@
+module Atomo
+  module AST
+    class Class < Rubinius::AST::Class
+      include NodeLike
+
+      def initialize(name, superclass, body)
+        @line = 1 # TODO
+        # super(1, name, superclass, body)
+
+        @superclass = superclass ? superclass : Primitive.new(:nil)
+
+        if name.kind_of?(Rubinius::AST::ClassName)
+          @name = name
+        else
+          @name = Rubinius::AST::ClassName.new @line, name.chain.last.to_sym, @superclass
+        end
+
+        @body = Rubinius::AST::ClassScope.new @line, @name, body
+      end
+
+      def recursively(stop = nil, &f)
+        return f.call self if stop and stop.call(self)
+
+        Class.new(
+          @name,
+          @superclass,
+          @body.body.recursively(stop, &f)
+        )
+      end
+    end
+  end
+end
