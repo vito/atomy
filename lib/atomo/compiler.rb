5ffc3543264dbdcd7396aaad31ef6ce9e9cc0bdd
Alex Suraci <i.am@toogeneric.com>
2011-02-27 20:10:09 -0500

    compiler util now loads the kernel

--- lib/atomo/compiler.rb
+++ lib/atomo/compiler.rb
@@ -1,9 +1,21 @@
 base = File.expand_path "../", __FILE__
+kernel = File.expand_path "../../../kernel/", __FILE__
 
 require base + "/compiler/compiler"
 require base + "/compiler/stages"
 require base + '/parser'
 require base + '/macros'
+
+Kernel.send(:define_method, :"load:") do |a|
+  cm = Atomo::Compiler.compile_file a
+
+  cm.create_script(false)
+
+  MAIN.__send__ :__script__
+end
+
+send(:"load:", kernel + "/core.atomo")
+send(:"load:", kernel + "/therie.atomo")
 
 cm = Atomo::Compiler.compile_string ARGV[0]
 
