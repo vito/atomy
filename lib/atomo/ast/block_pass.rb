e61a04b1cada1055db70d167892957ae6bed0533
Alex Suraci <i.am@toogeneric.com>
2011-03-04 13:54:01 -0500

    added BlockPass#recursively and BlockPass#construct

--- lib/atomo/ast/block_pass.rb
+++ lib/atomo/ast/block_pass.rb
@@ -6,12 +6,28 @@ module Atomo
       attr_reader :name
 
       def initialize(line, body)
-        super(1, body) # TODO
+        super
       end
 
       def ==(b)
         b.kind_of?(BlockPass) and \
         @body == b.body
+      end
+
+      def recursively(stop = nil, &f)
+        return f.call self if stop and stop.call(self)
+
+        BlockPass.new(
+          @line,
+          @body.recursively(stop, &f)
+        )
+      end
+
+      def construct(g, d)
+        get(g)
+        g.push_int @line
+        @body.construct(g, d)
+        g.send :new, 2
       end
     end
   end
