42211432399921e2ff1a9040c2a4b80f130bc353
Alex Suraci <i.am@toogeneric.com>
2011-03-13 00:14:55 -0500

    load patterns somewhere saner

--- lib/atomo/parser.rb
+++ lib/atomo/parser.rb
@@ -4,7 +4,6 @@ $:.unshift File.expand_path("../../vendor/kpeg/lib", __FILE__)
 base = File.expand_path "../", __FILE__
 
 require base + "/atomo.kpeg.rb"
-require base + "/patterns"
 
 module Atomo
   class Parser
