b4570fca52d43c162c8e96169127ed41ad762599
Alex Suraci <i.am@toogeneric.com>
2011-05-15 14:59:18 -0400

    macro-quote flags should be arguments to the string msg, not its receiver

--- lib/ast/string.rb
+++ lib/ast/string.rb
@@ -11,22 +11,12 @@ module Atomy
       end
 
       def as_message(send)
-        case send.receiver
-        when Send
-          MacroQuote.new(
-            @line,
-            send.receiver.method_name,
-            @raw || @value,
-            send.receiver.arguments.collect(&:name)
-          )
-        when Variable
-          MacroQuote.new(
-            @line,
-            send.receiver.name,
-            @raw || @value,
-            []
-          )
-        end
+        MacroQuote.new(
+          @line,
+          send.receiver.name,
+          @raw || @value,
+          send.arguments.collect(&:name)
+        )
       end
     end
   end
