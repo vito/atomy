7cce0bc8de5ee23ac38c11e23479d7209275e634
Alex Suraci <i.am@toogeneric.com>
2011-11-08 12:15:18 -0800

    simpler #require monkey-patching

--- lib/atomy/rubygems.rb
+++ lib/atomy/rubygems.rb
@@ -4,29 +4,13 @@
 Gem.suffixes << ".ay"
 
 module Kernel
-  # rubygems has patched things up already; sneak in there after it does
-  # its work by overriding what it thinks is the original #require
-  if defined? gem_original_require
-    alias atomy_original_require gem_original_require
+  alias atomy_original_require require
 
-    def gem_original_require(name)
-      if file = Atomy::CodeLoader.find_atomy(name)
-        Atomy::CodeLoader.load_file(file)
-      else
-        atomy_original_require(name)
-      end
-    end
-
-  # simpler route; just override the core #require to handle Atomy code
-  else
-    alias atomy_original_require require
-
-    def require(name)
-      if file = Atomy::CodeLoader.find_atomy(name)
-        Atomy::CodeLoader.load_file(file)
-      else
-        atomy_original_require(name)
-      end
+  def require(name)
+    if file = Atomy::CodeLoader.find_atomy(name)
+      Atomy::CodeLoader.load_file(file)
+    else
+      atomy_original_require(name)
     end
   end
 
