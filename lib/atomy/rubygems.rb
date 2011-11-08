1dfbd5642b35c70489f5ae8449d040d117db16d5
Alex Suraci <i.am@toogeneric.com>
2011-11-08 09:41:29 -0800

    update #require injection, hopefully fixes travis

--- lib/atomy/rubygems.rb
+++ lib/atomy/rubygems.rb
@@ -4,13 +4,29 @@
 Gem.suffixes << ".ay"
 
 module Kernel
-  alias atomy_original_require gem_original_require
+  # rubygems has patched things up already; sneak in there after it does
+  # its work by overriding what it thinks is the original #require
+  if defined? gem_original_require
+    alias atomy_original_require gem_original_require
 
-  def gem_original_require(name)
-    if file = Atomy::CodeLoader.find_atomy(name)
-      Atomy::CodeLoader.load_file(file)
-    else
-      atomy_original_require(name)
+    def gem_original_require(name)
+      if file = Atomy::CodeLoader.find_atomy(name)
+        Atomy::CodeLoader.load_file(file)
+      else
+        atomy_original_require(name)
+      end
+    end
+
+  # simpler route; just override the core #require to handle Atomy code
+  else
+    alias atomy_original_require require
+
+    def require(name)
+      if file = Atomy::CodeLoader.find_atomy(name)
+        Atomy::CodeLoader.load_file(file)
+      else
+        atomy_original_require(name)
+      end
     end
   end
 
