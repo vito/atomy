a7b3f13f649ba7e65dff257d974e3ccb94aeb520
Alex Suraci <suraci.alex@gmail.com>
2011-11-23 16:10:23 -0800

    remove script name from ARGV when

---
+++ test/bench/fib/fib.rb
@@ -0,0 +1,40 @@
+$LOAD_PATH << File.expand_path("../../rubinius/benchmark/lib", __FILE__)
+require("benchmark/ips")
+
+class Fixnum
+  def fib_cond
+    if self == 0
+      1
+    elsif self == 1
+      1
+    else
+      (self - 2).fib_cond + (self - 1).fib_cond
+    end
+  end
+end
+
+class Fixnum
+  def fib_atomy_1; 1; end
+  def fib_atomy_2; 1; end
+  def fib_atomy_3; (self - 2).fib_atomy + (self - 1).fib_atomy; end
+
+  def fib_atomy
+    if self == 0
+      fib_atomy_1
+    elsif self == 1
+      fib_atomy_2
+    else
+      fib_atomy_3
+    end
+  end
+end
+
+Benchmark.ips do |x|
+  x.report("20.fib_cond") do
+    20.fib_cond
+  end
+
+  x.report("20.fib_atomy") do
+    20.fib_atomy
+  end
+end
