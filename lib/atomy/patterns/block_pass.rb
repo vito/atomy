635e9fc39d43d1f3e4aca3854de2b2436d4a634f
Alex Suraci <suraci.alex@gmail.com>
2011-11-25 00:40:50 -0800

    BlockPass still assigns if value is nil

--- lib/atomy/patterns/block_pass.rb
+++ lib/atomy/patterns/block_pass.rb
@@ -13,24 +13,19 @@ module Atomy::Patterns
     end
 
     def deconstruct(g, locals = {})
-      nil_block = g.new_label
-      done = g.new_label
+      match = g.new_label
 
       g.dup
       g.is_nil
-      g.git nil_block
+      g.git match
 
       g.push_cpath_top
       g.find_const :Proc
       g.swap
       g.send :__from_block__, 1
+
+      match.set!
       @pattern.deconstruct(g, locals)
-      g.goto done
-
-      nil_block.set!
-      g.pop
-
-      done.set!
     end
   end
 end
