7b55c247ad6ff4b6735cb29c7806defce2471091
Alex Suraci <suraci.alex@gmail.com>
2011-11-27 15:04:37 -0800

    much much cleaner QuasiQuote#matches? and #deconstruct

--- lib/atomy/patterns/quasi_quote.rb
+++ lib/atomy/patterns/quasi_quote.rb
@@ -23,160 +23,11 @@ module Atomy::Patterns
       expression.get(g)
     end
 
-    def context(g, w, defaults, splices = 0)
-      w.each do |c|
-        if c.kind_of?(Array)
-          g.send c[0], 0
-
-          dflt = defaults[c[1]]
-
-          if dflt
-            valid = g.new_label
-            done = g.new_label
-
-            g.dup
-            g.send :size, 0
-            g.push_int(c[1] + 1)
-            g.send :>=, 1
-            g.git valid
-
-            g.pop
-            dflt.default.compile(g)
-            g.goto done
-
-            valid.set!
-          end
-
-          g.push_int(c[1] - splices)
-          g.send :[], 1
-
-          done.set! if dflt
-        else
-          g.send c, 0
-        end
-      end
-    end
-
-    def my_context(e, w)
-      x = e
-      w.each do |c|
-        if c.kind_of?(Array)
-          x = x.send(c[0])[c[1]]
-        else
-          x = x.send(c)
-        end
-      end
-      x
-    end
-
-    def required_size(ps)
-      req = 0
-      vary = false
-      defaults = {}
-      ps.each_with_index do |x, i|
-        if x.is_a?(Atomy::AST::Splice)
-          vary = true
-        elsif x.is_a?(Atomy::AST::Unquote) and \
-                (pat = x.expression.to_pattern).is_a?(Default)
-          vary = true
-          defaults[i] = pat
-        else
-          # more values after a splice indicates fixed width
-          vary = false if vary
-          req += 1
-        end
-      end
-      [req, vary, defaults]
-    end
-
     def matches?(g)
       mismatch = g.new_label
       done = g.new_label
 
-      them = g.new_stack_local
-      g.set_stack_local them
-      g.pop
-
-      where = nil
-      splice = false
-      splices = 0
-      defaults = {}
-
-      pre = proc { |e, c, d|
-        if c.kind_of?(Array) && c[1] == 0 &&
-            pats = my_context(@quoted.expression, (where + [c[0]]))
-          req, vary, defaults = required_size(pats)
-          g.push_stack_local them
-          context(g, where + [c[0]], defaults, splices)
-          g.send :size, 0
-          g.push_int req
-          g.send(vary ? :>= : :==, 1)
-          g.gif mismatch
-        end
-
-        where << c if c && where
-
-        where = [] if !where and c == :expression
-
-        if e.kind_of?(Atomy::AST::Splice)
-          splice = true
-          splices += 1
-        end
-
-        if !(e.unquote? && d == 1) && where && c != :unquoted
-          e.get(g)
-          g.push_stack_local them
-          context(g, where, defaults, splices)
-          g.kind_of
-          g.gif mismatch
-
-          e.details.each do |a|
-            val = e.send(a)
-            if val.kind_of?(Array)
-              val.each do |v|
-                g.push_literal v
-              end
-              g.make_array val.size
-            else
-              g.push_literal val
-            end
-            g.push_stack_local them
-            context(g, where, defaults, splices)
-            g.send a, 0
-            g.send :==, 1
-            g.gif mismatch
-          end
-
-          if e.bottom?
-            e.construct(g)
-            g.push_stack_local them
-            context(g, where, defaults, splices)
-            g.send :==, 1
-            g.gif mismatch
-          end
-        end
-
-        true
-      }
-
-      post = proc { where.pop }
-
-      @quoted.through_quotes(pre, post) do |e|
-        ctx = where.last == :unquoted ? where[0..-2] : where
-        g.push_stack_local them
-        if splice
-          context(g, ctx[0..-2], defaults, splices)
-          g.send ctx.last[0], 0
-          g.push_int ctx.last[1]
-          g.send :drop, 1
-          splice = false
-        else
-          context(g, ctx, defaults, splices)
-        end
-        e.to_pattern.matches?(g)
-        g.gif mismatch
-        e
-      end
+      Matcher.new(g, mismatch).go(@quoted.expression)
 
       g.push_true
       g.goto done
@@ -188,46 +39,7 @@ module Atomy::Patterns
     end
 
     def deconstruct(g, locals = {})
-      them = g.new_stack_local
-      g.set_stack_local them
-      g.pop
-
-      where = nil
-      splice = false
-      defaults = {}
-
-      pre = proc { |n, c|
-        if c.kind_of?(Array) && c[1] == 0 &&
-            pats = my_context(@quoted.expression, (where + [c[0]]))
-          _, _, defaults = required_size(pats)
-        end
-
-        where << c if c && where
-
-        where = [] if !where and c == :expression
-
-        splice = true if n.kind_of?(Atomy::AST::Splice)
-
-        true
-      }
-
-      post = proc { where.pop }
-
-      @quoted.through_quotes(pre, post) do |e|
-        ctx = where.last == :unquoted ? where[0..-2] : where
-        g.push_stack_local them
-        if splice &&
-          context(g, ctx[0..-2], defaults)
-          g.send ctx.last[0], 0
-          g.push_int ctx.last[1]
-          g.send :drop, 1
-          splice = false
-        else
-          context(g, ctx, defaults)
-        end
-        e.to_pattern.deconstruct(g)
-        e
-      end
+      Deconstructor.new(g).go(@quoted.expression)
     end
 
     def local_names
@@ -248,6 +60,233 @@ module Atomy::Patterns
       end
 
       false
+    end
+
+    class Walker
+      def initialize(g)
+        @depth = 1
+        @g = g
+      end
+
+      def go(x, mismatch = nil)
+        if mismatch
+          old, @mismatch = @mismatch, mismatch
+        end
+
+        x.accept self
+      ensure
+        @mismatch = old if mismatch
+      end
+
+      def quasiquote(qq)
+        @depth += 1
+        visit(qq)
+        @depth -= 1
+      end
+
+      def unquote(qq)
+        raise "unquote not overridden"
+      end
+
+      def splice(x)
+        raise "splice unquote outside of list context"
+      end
+
+      def push_literal(x)
+        case x
+        when Array
+          x.each do |v|
+            push_literal(x)
+          end
+          @g.make_array x.size
+        else
+          @g.push_literal x
+        end
+      end
+    end
+
+    class Matcher < Walker
+      def initialize(g, m)
+        super(g)
+        @mismatch = m
+      end
+
+      # effect on the stack: pop
+      def visit(x)
+        popmis = @g.new_label
+        popmis2 = @g.new_label
+        done = @g.new_label
+
+        @g.dup
+        x.get(@g)
+        @g.swap
+        @g.kind_of
+        @g.gif popmis
+
+        x.details.each do |n|
+          @g.dup
+          @g.send n, 0
+          push_literal x.send(n)
+          @g.send :==, 1
+          @g.gif popmis
+        end
+
+        # TODO: optionals
+
+        x.class.children[:required].each do |c|
+          @g.dup
+          @g.send c, 0
+          go(x.send(c), popmis)
+        end
+
+        x.class.children[:many].each do |c|
+          pats = x.send(c).dup
+
+          splice = pats.last && pats.last.splice? ? pats.pop : nil
+
+          # TODO: only handle trailing defaults
+          defaults, required = pats.partition do |x|
+            x.unquote? && x.expression.to_pattern.is_a?(Default)
+          end
+
+          @g.dup
+          @g.send c, 0
+          @g.dup
+          @g.send :size, 0
+          @g.push_int required.size
+          @g.send(splice || !defaults.empty? ? :>= : :==, 1)
+          @g.gif popmis2
+
+          required.each do |p|
+            @g.shift_array
+            go(p, popmis2)
+          end
+
+          defaults.each.with_index do |d, i|
+            has = @g.new_label
+            match = @g.new_label
+
+            @g.dup
+            @g.send :size, 0
+            @g.push_int(i + 1)
+            @g.send :>=, 1
+            @g.git has
+
+            d.expression.to_pattern.default.compile(@g)
+            @g.goto match
+
+            has.set!
+            @g.shift_array
+
+            match.set!
+            go(d, popmis2)
+          end
+
+          if splice
+            splice.expression.to_pattern.matches?(@g)
+            @g.gif popmis
+          else
+            @g.pop
+          end
+        end
+
+        @g.goto done
+
+        popmis2.set!
+        @g.pop
+
+        popmis.set!
+        @g.pop
+        @g.goto @mismatch
+
+        done.set!
+        @g.pop
+      end
+
+      def unquote(x)
+        @depth -= 1
+
+        if @depth == 0
+          x.expression.to_pattern.matches?(@g)
+          @g.gif @mismatch
+        else
+          visit(x)
+        end
+
+        @depth += 1
+      end
+    end
+
+    class Deconstructor < Walker
+      def unquote(x)
+        @depth -= 1
+
+        if @depth == 0
+          x.expression.to_pattern.deconstruct(@g)
+        else
+          visit(x)
+        end
+
+        @depth += 1
+      end
+
+      # effect on the stack: pop
+      def visit(x)
+        # TODO: optionals
+
+        x.class.children[:required].each do |c|
+          @g.dup
+          @g.send c, 0
+          go(x.send(c))
+        end
+
+        x.class.children[:many].each do |c|
+          pats = x.send(c).dup
+
+          splice = pats.last && pats.last.splice? ? pats.pop : nil
+
+          # TODO: only handle trailing defaults
+          defaults, required = pats.partition do |x|
+            x.unquote? && x.expression.to_pattern.is_a?(Default)
+          end
+
+          @g.dup
+          @g.send c, 0
+
+          required.each do |p|
+            @g.shift_array
+            go(p)
+          end
+
+          defaults.each.with_index do |d, i|
+            has = @g.new_label
+            match = @g.new_label
+
+            @g.dup
+            @g.send :size, 0
+            @g.push_int(i + 1)
+            @g.send :>=, 1
+            @g.git has
+
+            d.expression.to_pattern.default.compile(@g)
+            @g.goto match
+
+            has.set!
+            @g.shift_array
+
+            match.set!
+            go(d)
+          end
+
+          if splice
+            splice.expression.to_pattern.deconstruct(@g)
+          else
+            @g.pop
+          end
+        end
+
+        @g.pop
+      end
     end
   end
 end
