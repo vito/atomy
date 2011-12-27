c53234fd886ebd2caeff6b36e258d76c9fde6f84
Alex Suraci <suraci.alex@gmail.com>
2011-12-27 14:33:05 -0500

    remove dead code

--- lib/atomy/macros.rb
+++ lib/atomy/macros.rb
@@ -1,13 +1,8 @@
 module Atomy::Macro
   class Environment
     @@salt = 0
-    @@let = {}
 
     class << self
-      def let
-        @@let
-      end
-
       def salt
         @@salt
       end
@@ -16,42 +11,5 @@ module Atomy::Macro
         @@salt += n
       end
     end
-  end
-
-  def self.register(target, pattern, body, file = :macro, let = false)
-    name = let ? :"_let#{Environment.salt!}" : :_expand
-
-    Atomy::AST::Define.new(
-      0,
-      Atomy::AST::Compose.new(
-        0,
-        pattern.quoted,
-        Atomy::AST::Word.new(0, name)
-      ),
-      Atomy::AST::Send.new(
-        body.line,
-        Atomy::AST::Send.new(
-          body.line,
-          body,
-          [],
-          :to_node
-        ),
-        [],
-        :expand
-      )
-    ).evaluate(
-      Binding.setup(
-        TOPLEVEL_BINDING.variables,
-        TOPLEVEL_BINDING.code,
-        Rubinius::StaticScope.new(Atomy::AST)
-      ), file.to_s, pattern.quoted.line
-    )
-
-    if let
-      Environment.let[target] ||= []
-      Environment.let[target] << name
-    end
-
-    name
   end
 end
