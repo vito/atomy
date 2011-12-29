5823b83a7faa8accb573a5e93dd88fd770bfd640
Alex Suraci <suraci.alex@gmail.com>
2011-12-29 00:55:39 -0500

    adopt Ruby 1.9 block argument semantics

--- lib/atomy/ast/block.rb
+++ lib/atomy/ast/block.rb
@@ -1,5 +1,60 @@
 module Atomy
   module AST
+    class DefaultArguments
+      attr_accessor :arguments, :names
+
+      def initialize(line, defaults)
+        @line = line
+        @defaults = defaults
+      end
+
+      def map_arguments(scope)
+        @defaults.each do |d|
+          n, v = d
+          d << scope.new_local(n).reference
+        end
+      end
+
+      def bytecode(g)
+        @defaults.each do |n, a, r|
+          done = g.new_label
+
+          g.passed_arg r.slot
+          g.git done
+          a.compile(g)
+          g.pop
+
+          done.set!
+        end
+      end
+
+      def names
+        @defaults.collect do |d|
+          d[0]
+        end
+      end
+    end
+
+    class FormalArguments < Rubinius::AST::FormalArguments19
+      def initialize(line, required, optional, splat, post, block, patterns)
+        super(line, required, nil, splat, post, block)
+
+        if optional
+          @defaults = DefaultArguments.new line, optional
+          @optional = @defaults.names
+        end
+
+        @patterns = patterns
+      end
+
+      def set_patterns(g)
+        @patterns.each do |n, p|
+          g.state.scope.search_local(n).get_bytecode(g)
+          p.match(g)
+        end
+      end
+    end
+
     class Block < Rubinius::AST::Iter
       include NodeLike
       extend SentientNode
@@ -13,14 +68,43 @@ module Atomy
 
       alias :caller :body
 
+      def make_arguments
+        required = []
+        optional = nil
+        post = nil
+        splat = nil
+
+        locals = []
+
+        @arguments.collect(&:to_pattern).each.with_index do |p, i|
+          name = :"_arg:#{i + 1}"
+          locals << [name, p]
+
+          if p.is_a?(Patterns::Splat)
+            splat = name
+          elsif p.is_a?(Patterns::Default)
+            optional ||= []
+            optional << [name, p.default]
+          elsif splat
+            post ||= []
+            post << name
+          else
+            required << name
+          end
+        end
+
+        FormalArguments.new(@line, required, optional, splat, post, nil, locals)
+      end
+
       def bytecode(g)
         pos(g)
 
         state = g.state
         state.scope.nest_scope self
 
-        arguments = BlockArguments.new @arguments
-        blk = new_block_generator g, arguments
+        args = make_arguments
+
+        blk = new_block_generator g, args
 
         blk.push_state self
         blk.state.push_super state.super
@@ -28,15 +112,7 @@ module Atomy
 
         blk.state.push_name blk.name
 
-        # Push line info down.
         pos(blk)
-
-        if @block
-          blk.push_block_arg
-          @block.to_pattern.deconstruct(blk)
-        end
-
-        arguments.bytecode(blk)
 
         blk.state.push_block
         blk.push_modifiers
@@ -48,10 +124,20 @@ module Atomy
         too_few = blk.new_label
         done = blk.new_label
 
-        blk.passed_arg(arguments.required_args - 1)
+        blk.passed_arg(args.required_args - 1)
         blk.gif too_few
 
+        args.bytecode(blk)
+
+        args.set_patterns(blk)
+
+        if @block
+          blk.push_block_arg
+          @block.to_pattern.deconstruct(blk)
+        end
+
         body.compile(blk)
+
         blk.goto done
 
         too_few.set!
@@ -70,7 +156,7 @@ module Atomy
         blk.close
         blk.pop_state
 
-        blk.splat_index = arguments.splat_index
+        blk.splat_index = args.splat_index
         blk.local_count = local_count
         blk.local_names = local_names
 
@@ -80,68 +166,6 @@ module Atomy
         g.find_const :Proc
         g.swap
         g.send :__from_block__, 1
-      end
-    end
-
-    class BlockArguments
-      attr_reader :arguments
-
-      def initialize(args)
-        @arguments = args.collect(&:to_pattern)
-      end
-
-      def bytecode(g)
-        return if @arguments.empty?
-
-        g.cast_for_splat_block_arg
-        @arguments.each do |a|
-          if a.kind_of?(Patterns::Splat)
-            a.pattern.deconstruct(g)
-            return
-          else
-            g.shift_array
-            a.match(g)
-          end
-        end
-        g.pop
-      end
-
-      def local_names
-        @arguments.collect { |a| a.local_names }.flatten
-      end
-
-      def size
-        @arguments.size
-      end
-
-      def locals
-        local_names.size
-      end
-
-      def required_args
-        @arguments.reject { |a|
-          a.is_a?(Patterns::Default) || a.is_a?(Patterns::Splat) ||
-            a.is_a?(Patterns::BlockPass)
-        }.size
-      end
-
-      def total_args
-        if splat_index
-          size - 1
-        else
-          size
-        end
-      end
-
-      def splat_index
-        @arguments.each do |a,i|
-          return i if a.kind_of?(Patterns::Splat)
-        end
-        nil
-      end
-
-      def post_args
-        0
       end
     end
 
