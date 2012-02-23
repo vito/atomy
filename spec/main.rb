711dfdae2429fecbc13e1eca8e8444e23d7671a3
Alex Suraci <suraci.alex@gmail.com>
2012-02-23 03:11:15 -0800

    fix specs

--- spec/main.rb
+++ spec/main.rb
@@ -1,5 +1,5 @@
 require "minitest/autorun"
-require_relative("../lib/atomy")
+require_relative("../lib/atomy/boot")
 require_relative("suite/method")
 require_relative("suite/patterns")
 require_relative("suite/macros")
