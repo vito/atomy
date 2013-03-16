a08e24d15ed9ada50cbcb9a702889b586866fd25
Alex Suraci <suraci.alex@gmail.com>
2013-03-16 11:56:37 -0700

    implement Module#evaluate

--- lib/atomy/module.rb
+++ lib/atomy/module.rb
@@ -1,3 +1,5 @@
+require "atomy/compiler"
+
 module Atomy
   class Module < ::Module
     def initialize(&blk)
@@ -6,6 +8,20 @@ module Atomy
 
     def compile(gen, node)
       expand(node).bytecode(gen, self)
+    end
+
+    def evaluate(node)
+      binding =
+        Binding.setup(
+          Rubinius::VariableScope.of_sender,
+          Rubinius::CompiledCode.of_sender,
+          Rubinius::ConstantScope.of_sender,
+          self)
+
+      code = Atomy::Compiler.compile(node, self)
+      block = Atomy::Compiler.construct_block(code, binding)
+
+      block.call
     end
   end
 end
--- spec/atomy/module_spec.rb
+++ spec/atomy/module_spec.rb
@@ -3,20 +3,112 @@ require "spec_helper"
 require "atomy/module"
 
 describe Atomy::Module do
+  class SelfCode
+    def bytecode(gen, mod)
+      gen.push_self
+    end
+  end
+
+  class SendCode
+    def initialize(receiver, message, arguments = [])
+      @receiver = receiver
+      @message = message
+      @arguments = arguments
+    end
+
+    def bytecode(gen, mod)
+      if @receiver
+        mod.compile(gen, @receiver)
+      else
+        gen.allow_private
+        gen.push_self
+      end
+
+      @arguments.each do |arg|
+        mod.compile(gen, arg)
+      end
+
+      gen.send(@message, @arguments.size)
+    end
+  end
+
+  class IntegerCode
+    def initialize(value)
+      @value = value
+    end
+
+    def bytecode(gen, mod)
+      gen.push_int(@value)
+    end
+  end
+
+  class LiteralCode
+    def initialize(value)
+      @value = value
+    end
+
+    def bytecode(gen, mod)
+      gen.push_literal(@value)
+    end
+  end
+
+  subject do
+    described_class.new do
+      extend self
+
+      def expand(node)
+        case node
+        when Atomy::Grammar::AST::Word
+          if node.text == :self
+            return SelfCode.new
+          end
+        when Atomy::Grammar::AST::Apply
+          if node.node.is_a?(Atomy::Grammar::AST::Word)
+            return SendCode.new(nil, node.node.text, node.arguments)
+          end
+        when Atomy::Grammar::AST::Prefix
+          if node.node.is_a?(Atomy::Grammar::AST::Word)
+            return LiteralCode.new(node.node.text)
+          end
+        end
+
+        node
+      end
+    end
+  end
+
   describe "#expand" do
     context "when an expansion rule matched" do
-      it "returns the expanded node"
+      it "returns the expanded node" do
+        expect(subject.expand(ast("self"))).to be_a(SelfCode)
+      end
     end
 
     context "when NO expansion rule matched" do
-      it "returns the original node"
+      it "returns the original node" do
+        node = ast("foo")
+        expect(subject.expand(node)).to eq(node)
+      end
     end
   end
 
   describe "#evaluate" do
-    it "compiles the given expression"
-    it "executes the compiled code"
-    it "executes with the module as 'self'"
+    it "compiles the given expression using the module" do
+      expect(subject.evaluate(ast(".foo"))).to eq(:foo)
+    end
+
+    it "executes the compiled code" do
+      value = catch(:foo) do
+        expect(subject.evaluate(ast("throw(.foo, .called)"))).to eq(self)
+        :not_called
+      end
+
+      expect(value).to eq(:called)
+    end
+
+    it "executes with the 'self' of the caller" do
+      expect(subject.evaluate(ast("self"))).to eq(self)
+    end
   end
 
   describe "#compile" do
