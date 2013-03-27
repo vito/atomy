d16247b6118c36200b37b05c54c49e5d46f562ef
Alex Suraci <suraci.alex@gmail.com>
2013-03-26 18:22:17 -0700

    add node meta-helpers

---
+++ lib/atomy/node/meta.rb
@@ -0,0 +1,207 @@
+module Atomy::Grammar::AST
+  class Node
+    class << self
+      def basename
+        @basename ||= name.split("::").last.to_sym
+      end
+    end
+
+    def each_child
+    end
+
+    def each_attribute
+    end
+
+    def children
+      names = []
+
+      each_child do |name, _|
+        names << name
+      end
+
+      names
+    end
+
+    def attributes
+      names = []
+
+      each_attribute do |name, _|
+        names << name
+      end
+
+      names
+    end
+
+    def accept(x)
+      name = :"visit_#{self.class.basename.downcase}"
+
+      if x.respond_to?(name)
+        x.send(name, self)
+      else
+        x.visit(self)
+      end
+    end
+
+    # Recreate the node, calling the block for sub-nodes and using its return
+    # value in place of the node
+    def through
+      dup
+    end
+  end
+
+  class Sequence
+    def each_child
+      yield :nodes, @nodes
+    end
+
+    def through
+      self.class.new(nodes.collect { |n| yield n })
+    end
+  end
+
+  class Number
+    def each_attribute
+      yield :value, @value
+    end
+  end
+
+  class Literal
+    def each_attribute
+      yield :value, @value
+    end
+  end
+
+  class Quote
+    def each_child
+      yield :node, @node
+    end
+
+    def through
+      self.class.new(yield @node)
+    end
+  end
+
+  class QuasiQuote
+    def each_child
+      yield :node, @node
+    end
+
+    def through
+      self.class.new(yield @node)
+    end
+  end
+
+  class Unquote
+    def each_child
+      yield :node, @node
+    end
+
+    def through
+      self.class.new(yield @node)
+    end
+  end
+
+  class Constant
+    def each_attribute
+      yield :text, @text
+    end
+  end
+
+  class Word
+    def each_attribute
+      yield :text, @text
+    end
+  end
+
+  class Prefix
+    def each_attribute
+      yield :operator, @operator
+    end
+
+    def each_child
+      yield :node, @node
+    end
+
+    def through
+      self.class.new(yield(@node), @operator)
+    end
+  end
+
+  class Postfix
+    def each_attribute
+      yield :operator, @operator
+    end
+
+    def each_child
+      yield :node, @node
+    end
+
+    def through
+      self.class.new(yield(@node), @operator)
+    end
+  end
+
+  class Infix
+    def each_attribute
+      yield :operator, @operator
+    end
+
+    def each_child
+      yield :left, @left
+      yield :right, @right
+    end
+
+    def through
+      self.class.new(yield(@left), yield(@right), @operator)
+    end
+  end
+
+  class Block
+    def each_child
+      yield :nodes, @nodes
+    end
+
+    def through
+      self.class.new(@nodes.collect { |n| yield n })
+    end
+  end
+
+  class List
+    def each_child
+      yield :nodes, @nodes
+    end
+
+    def through
+      self.class.new(@nodes.collect { |n| yield n })
+    end
+  end
+
+  class Compose
+    def each_child
+      yield :left, @left
+      yield :right, @right
+    end
+
+    def through
+      self.class.new(yield(@left), yield(@right))
+    end
+  end
+
+  class Apply
+    def each_child
+      yield :node, @node
+      yield :arguments, @arguments
+    end
+
+    def through
+      self.class.new(yield(@node), @arguments.collect { |a| yield a })
+    end
+  end
+
+  class StringLiteral
+    def each_attribute
+      yield :value, @value
+      yield :raw, @raw
+    end
+  end
+end
---
+++ spec/atomy/node/meta_spec.rb
@@ -0,0 +1,189 @@
+require "spec_helper"
+
+require "atomy/node/equality"
+require "atomy/node/meta"
+
+describe Atomy::Grammar::AST::Node do
+  describe ".basename" do
+    it "returns the node class's unqualified name" do
+      expect(Atomy::Grammar::AST::QuasiQuote.basename).to eq(:QuasiQuote)
+    end
+  end
+
+  describe "#each_child" do
+    expectations = {
+      ast("1") => [],
+      ast("1.0") => [],
+      ast("'1") => [[:node, ast("1")]],
+      ast("`1") => [[:node, ast("1")]],
+      ast("~1") => [[:node, ast("1")]],
+      ast("A") => [],
+      ast("a") => [],
+      ast("!a") => [[:node, ast("a")]],
+      ast("a!") => [[:node, ast("a")]],
+      ast("a + b") => [[:left, ast("a")], [:right, ast("b")]],
+      ast("{ a }") => [[:nodes, [ast("a")]]],
+      ast("[a]") => [[:nodes, [ast("a")]]],
+      ast("a b") => [[:left, ast("a")], [:right, ast("b")]],
+      ast("a(b)") => [[:node, ast("a")], [:arguments, [ast("b")]]],
+      ast('"foo"') => []
+    }
+
+    expectations.each do |node, expected|
+      context "with a #{node.class.basename}" do
+        it "yields the proper nodes" do
+          actual = []
+
+          node.each_child do |name, child|
+            actual << [name, child]
+          end
+
+          expect(actual).to eq(expected)
+        end
+      end
+    end
+  end
+
+  describe "#each_attribute" do
+    expectations = {
+      ast("1") => [[:value, 1]],
+      ast("1.0") => [[:value, 1.0]],
+      ast("'1") => [],
+      ast("`1") => [],
+      ast("~1") => [],
+      ast("A") => [[:text, :A]],
+      ast("a") => [[:text, :a]],
+      ast("!a") => [[:operator, :"!"]],
+      ast("a!") => [[:operator, :"!"]],
+      ast("a + b") => [[:operator, :+]],
+      ast("{ a }") => [],
+      ast("[a]") => [],
+      ast("a b") => [],
+      ast("a(b)") => [],
+      ast('"foo"') => [[:value, "foo"], [:raw, "foo"]]
+    }
+
+    expectations.each do |node, expected|
+      context "with a #{node.class.basename}" do
+        it "yields the proper attributes" do
+          actual = []
+
+          node.each_attribute do |name, value|
+            actual << [name, value]
+          end
+
+          expect(actual).to eq(expected)
+        end
+      end
+    end
+  end
+
+  describe "#children" do
+    expectations = {
+      ast("1") => [],
+      ast("1.0") => [],
+      ast("'1") => [:node],
+      ast("`1") => [:node],
+      ast("~1") => [:node],
+      ast("A") => [],
+      ast("a") => [],
+      ast("!a") => [:node],
+      ast("a!") => [:node],
+      ast("a + b") => [:left, :right],
+      ast("{ a }") => [:nodes],
+      ast("[a]") => [:nodes],
+      ast("a b") => [:left, :right],
+      ast("a(b)") => [:node, :arguments],
+      ast('"foo"') => []
+    }
+
+    expectations.each do |node, expected|
+      context "with a #{node.class.basename}" do
+        it "has the proper attribute names" do
+          expect(node.children).to eq(expected)
+        end
+      end
+    end
+  end
+
+  describe "#attributes" do
+    expectations = {
+      ast("1") => [:value],
+      ast("1.0") => [:value],
+      ast("'1") => [],
+      ast("`1") => [],
+      ast("~1") => [],
+      ast("A") => [:text],
+      ast("a") => [:text],
+      ast("!a") => [:operator],
+      ast("a!") => [:operator],
+      ast("a + b") => [:operator],
+      ast("{ a }") => [],
+      ast("[a]") => [],
+      ast("a b") => [],
+      ast("a(b)") => [],
+      ast('"foo"') => [:value, :raw]
+    }
+
+    expectations.each do |node, expected|
+      context "with a #{node.class.basename}" do
+        it "has the proper attribute names" do
+          expect(node.attributes).to eq(expected)
+        end
+      end
+    end
+  end
+
+  describe "#accept" do
+    let(:node) { ast("`abc") }
+
+    context "when the visitor responds to #visit_(lowercase basename)" do
+      let(:visitor) do
+        Class.new do
+          def visit_quasiquote(x)
+            x.foo!
+          end
+        end
+      end
+
+      it "sends the message" do
+        node.should_receive(:foo!)
+        node.accept(visitor.new)
+      end
+    end
+
+    context "when the visitor does NOT respond to the specific message" do
+      let(:visitor) do
+        Class.new do
+          def visit(x)
+            x.foo!
+          end
+        end
+      end
+
+      it "sends #visit" do
+        node.should_receive(:foo!)
+        node.accept(visitor.new)
+      end
+    end
+  end
+
+  describe "#through" do
+    let(:node) { ast("`abc") }
+
+    it "reconstructs the node, yielding its children" do
+      expect(node.through { ast("123") }).to eq(ast("`123"))
+    end
+
+    it "yields the children in order" do
+      actual = []
+
+      ast("[a, b, c]").through do |child|
+        actual << child
+        child
+      end
+
+      expect(actual).to eq([ast("a"), ast("b"), ast("c")])
+    end
+  end
+end
