d82f69d32bcf146b2c4d92914317ec99c2600ac3
Alex Suraci <suraci.alex@gmail.com>
2015-06-03 14:10:58 -0700

    assignment now performs pattern-matching again

--- lib/atomy/code/assign.rb
+++ lib/atomy/code/assign.rb
@@ -1,15 +1,81 @@
 module Atomy
   module Code
     class Assign
-      def initialize(name, value)
-        @name = name
+      def initialize(pattern, value)
+        @pattern = pattern
         @value = value
       end
 
       def bytecode(gen, mod)
+        pattern = mod.pattern(@pattern)
+
+        vars = []
+        pattern.locals.each do |name|
+          vars << assignment_local(gen, name)
+        end
+
+        # [value]
         mod.compile(gen, @value)
-        assignment_local(gen, @name).set_bytecode(gen)
+        # [value, value]
+        gen.dup
+        # [pattern, value, value]
+        mod.compile(gen, pattern)
+        # [value, pattern, value]
+        gen.swap
+
+        # [value, pattern, value, pattern, value]
+        gen.dup_many(2)
+
+        mismatch = gen.new_label
+        done = gen.new_label
+
+        # [bool, value, pattern, value]
+        gen.send(:matches?, 1)
+
+        # [value, pattern, value]
+        gen.gif(mismatch)
+
+        # [[bindings], value]
+        gen.send(:bindings, 1)
+
+        vars.each do |v|
+          # [binding, [bindings], value]
+          gen.shift_array
+
+          # [binding, [bindings], value]
+          v.set_bytecode(gen)
+
+          # [[bindings], value]
+          gen.pop
+        end
+
+        # [value]
+        gen.pop
+
+        # [value]
+        gen.goto(done)
+
+        # [value, pattern, value]
+        mismatch.set!
+
+        # [::, value, pattern, value]
+        gen.push_cpath_top
+        # [::Atomy, value, pattern, value]
+        gen.find_const(:Atomy)
+        # [::Atomy::PatternMismatch, value, pattern, value]
+        gen.find_const(:PatternMismatch)
+        # [value, pattern, ::Atomy::PatternMismatch, value]
+        gen.move_down(2)
+        # [#<Atomy::PatternMismatch>, value]
+        gen.send(:new, 2)
+        # exception
+        gen.raise_exc
+
+        # [value]
+        done.set!
       end
+
+      private
 
       def assignment_local(gen, name, set = false)
         var = gen.state.scope.search_local(name)
--- spec/atomy/code/assign_spec.rb
+++ spec/atomy/code/assign_spec.rb
@@ -6,24 +6,34 @@ require "atomy/code/assign"
 describe Atomy::Code::Assign do
   let(:compile_module) { Atomy::Module.new { use Atomy::Bootstrap } }
 
-  subject { described_class.new(name, value) }
-
-  let(:name) { :a }
+  let(:pattern) { ast("a") }
   let(:value) { ast("1") }
+
+  subject { described_class.new(pattern, value) }
 
   it "returns the matched value" do
     a = nil
     expect(compile_module.evaluate(subject)).to eq(1)
   end
 
-  it "assigns the variable in the current scope" do
+  it "pattern-matches the value, assigning locals in the current scope" do
     a = :unmodified
 
     expect(compile_module.evaluate(Atomy::Code::Sequence.new([
       subject,
-      Atomy::Code::Variable.new(name),
+      Atomy::Code::Variable.new(:a),
     ]))).to eq(1)
 
     expect(a).to eq(:unmodified)
+  end
+
+  context "when the pattern does not match" do
+    let(:pattern) { ast("2") }
+
+    it "raises Atomy::PatternMismatch" do
+      expect {
+        compile_module.evaluate(subject)
+      }.to raise_error(Atomy::PatternMismatch)
+    end
   end
 end
