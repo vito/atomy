2cd73337d969b220c6a14475b840c99b622a37bb
Alex Suraci <suraci.alex@gmail.com>
2015-06-06 16:47:26 -0700

    implement hash literals
    this uses a pretty clever way to be hygienic, which will probably be
    worth pulling out into its own module in the future

---
+++ spec/kernel/hash_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+
+require "atomy/codeloader"
+
+describe "hash kernel" do
+  subject { Atomy::Module.new { use(require_kernel("hash")) } }
+
+  it "implements hash literals" do
+    expect(subject.evaluate(ast('#{ .a -> 1, .b -> 2, 3 -> 4 }'))).to eq({
+      :a => 1,
+      :b => 2,
+      3 => 4,
+    })
+  end
+end
