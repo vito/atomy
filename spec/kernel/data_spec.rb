dafe31ea9f6c708298f92c17532a37214400b0a1
Alex Suraci <suraci.alex@gmail.com>
2015-06-07 11:04:35 -0700

    add data module for defining data hierarchies
    not as cool as the original one yet; can't define arbitrary patterns in
    arg list, has to be instance variables for them to be considered
    attributes

---
+++ spec/kernel/data_spec.rb
@@ -0,0 +1,186 @@
+require "spec_helper"
+
+require "atomy/codeloader"
+
+describe "data kernel" do
+  subject { Atomy::Module.new { use(require_kernel("data")) } }
+
+  it "returns nil" do
+    expect(subject.evaluate(ast("data(Point(@x, @y))"), subject.compile_context)).to be_nil
+  end
+
+  class DataParent
+  end
+
+  def self.it_defines(parent, klass, attrs = {})
+    describe "the defined #{klass}" do
+      let(:defined_klass) { subject.const_get(klass) }
+
+      it "is constructed with the given arguments" do
+        defined_klass.new(*attrs.values) # depends on hash order
+      end
+
+      it "has the correct superclass" do
+        superclass = parent
+        if superclass.is_a?(Symbol)
+          superclass = subject.const_get(superclass)
+        end
+
+        expect(defined_klass.direct_superclass).to eq(superclass)
+      end
+
+      it "defines attribute accessors for each instance variable" do
+        point = defined_klass.new(*attrs.values)
+
+        attrs.each do |a, v|
+          expect(point.public_send(a)).to eq(v)
+        end
+      end
+
+      it "defines a prettier #inspect" do
+        args = attrs.values.collect(&:to_s).join(", ")
+        expect(defined_klass.new(*attrs.values).inspect).to eq("#{klass}(#{args})")
+      end
+
+      it "can be pattern-matched" do
+        pats = attrs.keys.collect(&:to_s).join(", ")
+        args = attrs.values.collect(&:to_s).join(", ")
+        expect(subject.evaluate(seq("
+          #{klass}(#{pats}) = #{klass} new(#{args})
+          [#{pats}]
+        "), subject.compile_context)).to eq(attrs.values)
+      end
+
+      unless attrs.empty?
+        context "when infinitely recursive" do
+          it "pretty-prints with dots at the recursion point" do
+            point = defined_klass.new(*attrs.values)
+            point.public_send(:"#{attrs.keys.first}=", point)
+            value_args = attrs.values.collect(&:to_s)
+            value_args[0] = "#{klass}(...)"
+            values = value_args.join(", ")
+            expect(point.inspect).to eq("#{klass}(#{values})")
+          end
+        end
+      end
+    end
+  end
+
+  describe "a class defined with no children and no parent" do
+    before do
+      subject.evaluate(ast("data(Point(@x, @y))"), subject.compile_context)
+    end
+
+    it_defines Object, :Point, x: 1, y: 2
+  end
+
+  describe "a class defined with a parent" do
+    before do
+      subject.evaluate(ast("DataParent data(Point(@x, @y))"), subject.compile_context)
+    end
+
+    it_defines DataParent, :Point, x: 1, y: 2
+  end
+
+  describe "a class defined with children" do
+    before do
+      subject.evaluate(ast("
+        data(Shape(@area)):
+          Square(@width, @height)
+          Triangle(@a, @b, @c)
+      "), subject.compile_context)
+    end
+
+    it_defines Object, :Shape, area: 42
+    it_defines :Shape, :Square, width: 20, height: 20
+    it_defines :Shape, :Triangle, a: 1, b: 2, c: 3
+  end
+
+  describe "many classes defined in one block" do
+    before do
+      subject.evaluate(ast("
+        data:
+          Square(@width, @height)
+          Rectangle(@width, @height)
+      "), subject.compile_context)
+    end
+
+    it_defines Object, :Square, width: 20, height: 20
+    it_defines Object, :Rectangle, width: 30, height: 40
+  end
+
+  describe "many classes defined in one block with a parent" do
+    before do
+      subject.evaluate(ast("
+        DataParent data:
+          Square(@width, @height)
+          Rectangle(@width, @height)
+      "), subject.compile_context)
+    end
+
+    it_defines DataParent, :Square, width: 20, height: 20
+    it_defines DataParent, :Rectangle, width: 30, height: 40
+  end
+
+  describe "defining a class hierarchy" do
+    before do
+      subject.evaluate(ast("
+        data(Shape):
+          Rectangle(@width, @height):
+            Square(@width, @height)
+
+          Triangle:
+            Isosceles(@a, @b, @c)
+            Right
+      "), subject.compile_context)
+    end
+
+    it_defines Object, :Shape
+    it_defines :Shape, :Rectangle, width: 30, height: 40
+    it_defines :Rectangle, :Square, width: 20, height: 20
+    it_defines :Shape, :Triangle
+    it_defines :Triangle, :Isosceles, a: 1, b: 2, c: 3
+    it_defines :Triangle, :Right
+  end
+
+  describe "defining a class hierarchy with a parent" do
+    before do
+      subject.evaluate(ast("
+        DataParent data(Shape):
+          Rectangle(@width, @height):
+            Square(@width, @height)
+
+          Triangle:
+            Isosceles(@a, @b, @c)
+            Right
+      "), subject.compile_context)
+    end
+
+    it_defines DataParent, :Shape
+    it_defines :Shape, :Rectangle, width: 30, height: 40
+    it_defines :Rectangle, :Square, width: 20, height: 20
+    it_defines :Shape, :Triangle
+    it_defines :Triangle, :Isosceles, a: 1, b: 2, c: 3
+    it_defines :Triangle, :Right
+  end
+
+  describe "defining a class hierarchy with a parent and no initial parent" do
+    before do
+      subject.evaluate(ast("
+        DataParent data:
+          Rectangle(@width, @height):
+            Square(@width, @height)
+
+          Triangle:
+            Isosceles(@a, @b, @c)
+            Right
+      "), subject.compile_context)
+    end
+
+    it_defines DataParent, :Rectangle, width: 30, height: 40
+    it_defines :Rectangle, :Square, width: 20, height: 20
+    it_defines DataParent, :Triangle
+    it_defines :Triangle, :Isosceles, a: 1, b: 2, c: 3
+    it_defines :Triangle, :Right
+  end
+end
