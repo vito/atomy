03a2af45a7dbe511ce7a293f65cc4485607a696d
Alex Suraci <suraci.alex@gmail.com>
2015-06-06 20:14:59 -0700

    add with(expr, pat) pattern
    also change patterns kernel specs to be higher-level

--- spec/kernel/patterns_spec.rb
+++ spec/kernel/patterns_spec.rb
@@ -13,83 +13,46 @@ end
 describe "patterns kernel" do
   subject { Atomy::Module.new { use(require_kernel("patterns")) } }
 
-  describe "#pattern" do
-    context "with a Compose node with a Constant as the right-hand side" do
-      let(:node) { ast("PatternsKernelModule Blah") }
+  it "defines a pattern for nested consts" do
+    expect(subject.evaluate(ast("PatternsKernelModule Blah = PatternsKernelModule Blah new"))).to be_a(PatternsKernelModule::Blah)
+    expect { subject.evaluate(ast("PatternsKernelModule Blah = Object new")) }.to raise_error(Atomy::PatternMismatch)
+  end
 
-      it "expands it into a KindOf pattern" do
-        expanded = subject.evaluate(subject.pattern(node))
-        expect(expanded).to be_a(Atomy::Pattern::KindOf)
-        expect(expanded.klass).to eq(PatternsKernelModule::Blah)
-      end
+  it "defines a pattern for assigning object attributes" do
+    klass = Class.new { attr_accessor :y }
+    x = klass.new
+    expect(subject.evaluate(ast("x y = 1"))).to eq(1)
+    expect(x.y).to eq(1)
+  end
 
-      it "declares no locals" do
-        expect(subject.pattern(node).locals).to be_empty
-      end
-    end
+  it "defines a pattern for assigning indices" do
+    x = [1, 2, 3, 4]
+    expect(subject.evaluate(ast("x [1, 2] = 42"))).to eq(42)
+    expect(x).to eq([1, 42, 4])
+  end
 
-    context "with a Compose node with a Word as the right-hand side" do
-      let(:node) { ast("42 foo") }
+  it "defines a pattern for setting instance variables" do
+    expect(subject.evaluate(ast("@foo = 42"))).to eq(42)
+    expect(@foo).to eq(42)
+  end
 
-      it "expands it into an Attribute pattern" do
-        expanded = subject.evaluate(subject.pattern(node))
-        expect(expanded).to be_a(Atomy::Pattern::Attribute)
-        expect(expanded.receiver).to eq(42)
-        expect(expanded.attribute).to eq(:foo)
-      end
+  it "defines an or pattern" do
+    expect(subject.evaluate(seq("((a & 1) | (b & 2)) = 1, [a, b]"))).to eq([1, nil])
+    expect(subject.evaluate(seq("((c & 1) | (d & 2)) = 2, [c, d]"))).to eq([nil, 2])
+    expect { subject.evaluate(ast("((c & 1) | (d & 2)) = 3")) }.to raise_error(Atomy::PatternMismatch)
+  end
 
-      it "declares no locals" do
-        expect(subject.pattern(node).locals).to be_empty
-      end
-    end
+  it "defines a 'with' pattern for matching evaluations on an object" do
+    klass = Class.new { attr_accessor :foo }
+    x = klass.new
 
-    context "with a Compose node with a List as the right-hand side" do
-      let(:node) { ast("[1, 2, 3] [4, 5]") }
+    x.foo = 1
+    expect(subject.evaluate(seq("with(@foo, a) = x, a"))).to eq(1)
 
-      it "expands it into an Index pattern" do
-        expanded = subject.evaluate(subject.pattern(node))
-        expect(expanded).to be_a(Atomy::Pattern::Index)
-        expect(expanded.receiver).to eq([1, 2, 3])
-        expect(expanded.arguments).to eq([4, 5])
-      end
+    x.foo = 2
+    expect(subject.evaluate(seq("with(@foo, a & 2) = x, a"))).to eq(2)
 
-      it "declares no locals" do
-        expect(subject.pattern(node).locals).to be_empty
-      end
-    end
-
-    context "with an instance variable" do
-      let(:node) { ast("@foo") }
-
-      it "expands it into an InstanceVariable pattern" do
-        expanded = subject.evaluate(subject.pattern(node))
-        expect(expanded).to be_a(Atomy::Pattern::InstanceVariable)
-        expect(expanded.name).to eq(:foo)
-      end
-
-      it "declares no locals" do
-        expect(subject.pattern(node).locals).to be_empty
-      end
-    end
-
-    context "with a | b" do
-      let(:node) { ast("a | (b & 2)") }
-
-      it "expands it into an InstanceVariable pattern" do
-        expanded = subject.evaluate(subject.pattern(node))
-        expect(expanded).to be_a(Atomy::Pattern::Or)
-        expect(expanded.a).to be_a(Atomy::Pattern::Wildcard)
-        expect(expanded.a.name).to eq(:a)
-        expect(expanded.b).to be_a(Atomy::Pattern::And)
-        expect(expanded.b.a).to be_a(Atomy::Pattern::Wildcard)
-        expect(expanded.b.a.name).to eq(:b)
-        expect(expanded.b.b).to be_a(Atomy::Pattern::Equality)
-        expect(expanded.b.b.value).to eq(2)
-      end
-
-      it "declares locals of both branches" do
-        expect(subject.pattern(node).locals).to eq([:a, :b])
-      end
-    end
+    x.foo = 3
+    expect { subject.evaluate(seq("with(@foo, a & 2) = x, a")) }.to raise_error(Atomy::PatternMismatch)
   end
 end
