43f01b327dd43e27af44e134707bd6ae8f3ffd1b
Alex Suraci <suraci.alex@gmail.com>
2015-06-09 13:05:35 -0700

    add range kernel

---
+++ spec/kernel/range_spec.rb
@@ -0,0 +1,12 @@
+require "spec_helper"
+
+require "atomy/codeloader"
+
+describe "range kernel" do
+  subject { Atomy::Module.new { use(require_kernel("range")) } }
+
+  it "implements range literals" do
+    expect(subject.evaluate(ast('1 .. 2'))).to eq(1 .. 2)
+    expect(subject.evaluate(ast('1 ... 2'))).to eq(1 ... 2)
+  end
+end
