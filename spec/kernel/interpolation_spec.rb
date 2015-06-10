1de86a420050aa04d79c3506479a81fd9aa2cab9
Alex Suraci <suraci.alex@gmail.com>
2015-06-10 10:13:00 -0700

    add interpolation and grammar kernel modules

---
+++ spec/kernel/interpolation_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+require "atomy/codeloader"
+
+describe "interpolation kernel" do
+  subject { Atomy::Module.new { use(require_kernel("interpolation")) } }
+
+  it "implements string interpolation literals" do
+    expect(subject.evaluate(ast('i"foo bar"'))).to eq("foo bar")
+    expect(subject.evaluate(ast('i"foo #{1 + 1} bar"'))).to eq("foo 2 bar")
+  end
+
+  it "implements symbol interpolation literals" do
+    expect(subject.evaluate(ast('."foo bar"'))).to eq(:"foo bar")
+    expect(subject.evaluate(ast('."foo #{1 + 1} bar"'))).to eq(:"foo 2 bar")
+  end
+end
