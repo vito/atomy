cd3e0c4136e76b44703fdcbc8fcf0e01c506a3dd
Alex Suraci <suraci.alex@gmail.com>
2015-06-11 14:14:49 -0700

    implement let-macro

---
+++ spec/kernel/let_macro_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+
+require "atomy/codeloader"
+
+describe "let-macro kernel" do
+  subject { Atomy::Module.new { use(require_kernel("let-macro")) } }
+
+  it "defines the given macros for the duration of the body" do
+    expect(subject.evaluate(seq(<<EOF))).to eq([5, 4])
+val = let-macro((2 + 2) = '5): 2 + 2
+[val, 2 + 2]
+EOF
+  end
+
+  it "has the macros in-scope of each other" do
+    expect(subject.evaluate(seq(<<EOF))).to eq([5, 4, 7])
+val = let-macro((2 + 2) = '5, (3 + 4) = '(2 + 2)): 3 + 4
+[val, 2 + 2, 3 + 4]
+EOF
+  end
+end
