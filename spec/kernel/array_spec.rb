3f26d11f3fd22fe03642f546a05acf577cab19cd
Alex Suraci <suraci.alex@gmail.com>
2015-06-11 13:37:21 -0700

    add array kernel

---
+++ spec/kernel/array_spec.rb
@@ -0,0 +1,16 @@
+require "spec_helper"
+
+require "atomy/codeloader"
+
+describe "array kernel" do
+  subject { Atomy::Module.new { use(require_kernel("array")) } }
+
+  it "implements array consing syntax" do
+    expect(subject.evaluate(ast('1 . [2, 3]'))).to eq([1, 2, 3])
+  end
+
+  it "implements array de-consing pattern-matching syntax" do
+    expect(subject.evaluate(seq('(a . bs) = [1, 2, 3], [a, bs]'))).to eq([1, [2, 3]])
+    expect { subject.evaluate(seq('(a . bs) = [], [a, bs]')) }.to raise_error(Atomy::PatternMismatch)
+  end
+end
