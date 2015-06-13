de81d6d2cb016934e7bf2f586f1de1ac02cdf8ec
Alex Suraci <suraci.alex@gmail.com>
2015-06-12 17:56:43 -0700

    add rubygems shim
    ported from old atomy; probably warrants another look, but it at least
    works

---
+++ lib/atomy/rubygems.rb
@@ -0,0 +1,44 @@
+# Patch up RubyGems/require (indirectly) to support loading
+# Atomy gems
+
+Gem.suffixes << ".ay"
+
+module Kernel
+  alias atomy_original_gem_original_require gem_original_require
+  alias atomy_original_require require
+
+  def require(name)
+    if file = Atomy::CodeLoader.find_source(name)
+      Atomy::CodeLoader.require(file)
+    else
+      atomy_original_require(name)
+    end
+  end
+
+  def gem_original_require(name)
+    if file = Atomy::CodeLoader.find_source(name)
+      Atomy::CodeLoader.require(file)
+    else
+      atomy_original_gem_original_require(name)
+    end
+  end
+
+  module_function :require
+  module_function :gem_original_require
+
+  private :atomy_original_gem_original_require
+  private :atomy_original_require
+end
+
+# this is here so it's more likely to be filtered out of caller checks
+#
+# (e.g. sinatra/base)
+class Atomy::Module
+  def require(path)
+    if path.start_with? "./"
+      Kernel.require(File.expand_path("../" + path, @file.to_s))
+    else
+      Kernel.require(path)
+    end
+  end
+end
